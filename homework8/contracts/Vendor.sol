8988554dc4ab60522e3f174ffd52d7e1bcac0eb0
Sasha Grin <aleksander.g@ideasoft.io>
2021-12-16 17:56:17 +0200

    homework8 - some improvements for contract #2

--- homework8/contracts/Vendor.sol
+++ homework8/contracts/Vendor.sol
@@ -39,9 +39,9 @@ contract Vendor is Initializable, OwnableUpgradeable, UUPSUpgradeable   {
     event Log(string message);
     event LogBytes(bytes message);
 
-    address studentsContractAddress = 0x0E822C71e628b20a35F8bCAbe8c11F274246e64D;
-    address aggregatorAddressFor_ETH_USD = 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e;
-    address aggregatorAddressFor_DAI_USD = 0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF;
+    address studentsContractAddress;
+    address aggregatorAddressFor_ETH_USD;
+    address aggregatorAddressFor_DAI_USD;
     address DAITokenContractAddress;
     address myTokenContractAddress;
     address NFTTokenContractAddress;
@@ -96,7 +96,7 @@ contract Vendor is Initializable, OwnableUpgradeable, UUPSUpgradeable   {
         IERC20 myTokenContract = IERC20(myTokenContractAddress);
         require(amountToBuy > 0, "Maybe you would like to buy something greater than 0?");
 
-        uint256 amountOfDAITokensToPay = amountToBuy/uint256(getLatestPrice(aggregatorAddressFor_DAI_USD));
+        uint256 amountOfDAITokensToPay = amountToBuy;
 
         require(DAITokenContract.balanceOf(msg.sender) >= amountOfDAITokensToPay, "Sorry, you do not have enough DAI-tokens for swap");
         require(myTokenContract.balanceOf(address(this)) >= amountToBuy, "Sorry, there is not enough tokens on my balance");
@@ -104,26 +104,15 @@ contract Vendor is Initializable, OwnableUpgradeable, UUPSUpgradeable   {
         uint256 allowance = DAITokenContract.allowance(msg.sender, address(this));
         require(allowance >= amountToBuy, "Check the token allowance please");
 
-        try DAITokenContract.transferFrom(msg.sender, address(this), amountToBuy) {
-            emit Log("DAItokens transferedFrom");
-        } catch Error(string memory reason) {
-            emit Log(reason);
-        } catch (bytes memory reason) {
-            emit LogBytes(reason);
-        }
+        DAITokenContract.transferFrom(msg.sender, address(this), amountToBuy);
+        myTokenContract.transfer(msg.sender, amountToBuy);
 
-        try myTokenContract.transfer(msg.sender, amountToBuy) {
-            emit Log("tokens transfered");
-        } catch Error(string memory reason) {
-            emit Log(reason);
-        } catch (bytes memory reason) {
-            emit LogBytes(reason);
-        }
     }
 
-    function getLatestPrice(address aggregatorAddress) internal view returns (uint256){
+    function getLatestPrice(address aggregatorAddress) internal returns (uint256){
         (,int price,,,) = AggregatorV3Interface(aggregatorAddress).latestRoundData();
         uint8 decimals = AggregatorV3Interface(aggregatorAddress).decimals();
+
         return uint256(price)/10**decimals;
     }
 
--- homework8/test/Vendor.test.ts
+++ homework8/test/Vendor.test.ts
@@ -16,9 +16,10 @@ describe('Vendor', () => {
   let payer: any;
   let testTokenInstance: any;
   let vendorInstance: any;
+  let vendorInstanceForProxy: any;
   let vendorV2Instance: any;
   let DAITokenInstance: any;
-  let nftTockenInstance: any;
+  let nftTokenInstance: any;
   let proxyInstance: any;
   let myProxyInstance: any;
 
@@ -32,21 +33,134 @@ describe('Vendor', () => {
 
     testTokenInstance = await TestToken.new(10000);
     DAITokenInstance = await DAIMockToken.new(50);
-    nftTockenInstance = await MockNFTToken.new(nftTokenId);
+    nftTokenInstance = await MockNFTToken.new(nftTokenId);
     vendorInstance = await Vendor.new();
+    vendorInstanceForProxy = await Vendor.new();
     vendorV2Instance = await VendorV2.new();
 
-    const data = await vendorInstance.contract.methods
-      .initialize(testTokenInstance.address, DAITokenInstance.address, nftTockenInstance.address, nftTokenId)
-      .encodeABI();
-    proxyInstance = await Proxy.new(vendorInstance.address, data);
+    await vendorInstance.initialize(
+      testTokenInstance.address,
+      DAITokenInstance.address,
+      nftTokenInstance.address,
+      nftTokenId,
+    );
 
-    myProxyInstance = await new web3.eth.Contract(vendorInstance.abi, proxyInstance.address);
+    const data = await vendorInstanceForProxy.contract.methods
+      .initialize(testTokenInstance.address, DAITokenInstance.address, nftTokenInstance.address, nftTokenId)
+      .encodeABI();
+    proxyInstance = await Proxy.new(vendorInstanceForProxy.address, data);
+
+    myProxyInstance = await new web3.eth.Contract(vendorInstanceForProxy.abi, proxyInstance.address);
 
     await testTokenInstance.transfer(proxyInstance.address, web3.utils.toBN(5).mul(bn1e18));
+    await testTokenInstance.transfer(vendorInstance.address, web3.utils.toBN(5).mul(bn1e18));
   });
 
   describe('Buy tokens with ETH', function () {
+    it('Should buyTokens with ETH successfully', async () => {
+      const tokenBalanceBefore = await testTokenInstance.balanceOf(owner);
+      const vendorTokenBalanceBefore = await testTokenInstance.balanceOf(vendorInstance.address);
+
+      await vendorInstance.buyTokens({ from: owner, value: paymentAmount });
+
+      const vendorTokenBalanceAfter = await testTokenInstance.balanceOf(vendorInstance.address);
+      const tokenBalanceAfter = await testTokenInstance.balanceOf(owner);
+
+      assert.notEqual(web3.utils.toBN(0), vendorTokenBalanceBefore.sub(vendorTokenBalanceAfter));
+      assert.equal(
+        true,
+        tokenBalanceBefore.eq(tokenBalanceAfter.sub(vendorTokenBalanceBefore.sub(vendorTokenBalanceAfter))),
+      );
+    });
+
+    it('Should get back ether if there is not enough Vendor token balance', async () => {
+      const ethBalanceBefore = await web3.eth.getBalance(owner);
+      const result = await vendorInstance.buyTokens({ from: owner, value: paymentAmount.mul(web3.utils.toBN(1000)) });
+      const ethBalanceAfter = await web3.eth.getBalance(owner);
+      const transaction = await web3.eth.getTransaction(result.tx);
+
+      assert.equal(
+        true,
+        web3.utils
+          .toBN(result.receipt.gasUsed)
+          .mul(web3.utils.toBN(transaction.gasPrice))
+          .eq(web3.utils.toBN(ethBalanceBefore).sub(web3.utils.toBN(ethBalanceAfter))),
+      );
+    });
+
+    it("Should throw an error if msg.sender is not an owner of 'key NFT token'", async () => {
+      await truffleAssert.reverts(
+        vendorInstance.buyTokens({ from: payer, value: paymentAmount }),
+        "Sorry, you don't have a key to use this.",
+      );
+    });
+  });
+
+  describe('Buy tokens with DAI', function () {
+    it('Should swap Tokens for DAI successfully', async () => {
+      await testTokenInstance.transfer(vendorInstance.address, web3.utils.toBN(500).mul(bn1e18));
+      await DAITokenInstance.approve(vendorInstance.address, web3.utils.toBN(50).mul(bn1e18));
+      const tokenBalanceBefore = await testTokenInstance.balanceOf(owner);
+      const vendorTokenBalanceBefore = await testTokenInstance.balanceOf(vendorInstance.address);
+
+      const DAIBalanceBefore = await DAITokenInstance.balanceOf(owner);
+      const vendorDAIBalanceBefore = await DAITokenInstance.balanceOf(vendorInstance.address);
+
+      await vendorInstance.buyTokensForDAI(web3.utils.toBN(3).mul(bn1e18));
+
+      const vendorTokenBalanceAfter = await testTokenInstance.balanceOf(vendorInstance.address);
+      const tokenBalanceAfter = await testTokenInstance.balanceOf(owner);
+
+      const DAIBalanceAfter = await DAITokenInstance.balanceOf(owner);
+      const vendorDAIBalanceAfter = await DAITokenInstance.balanceOf(vendorInstance.address);
+
+      assert.notEqual(web3.utils.toBN(0), vendorTokenBalanceBefore.sub(vendorTokenBalanceAfter));
+      assert.equal(
+        true,
+        tokenBalanceBefore.eq(tokenBalanceAfter.sub(vendorTokenBalanceBefore.sub(vendorTokenBalanceAfter))),
+      );
+
+      assert.notEqual(web3.utils.toBN(0), vendorDAIBalanceBefore.sub(vendorDAIBalanceAfter));
+      assert.equal(true, DAIBalanceBefore.eq(DAIBalanceAfter.sub(vendorDAIBalanceBefore.sub(vendorDAIBalanceAfter))));
+    });
+
+    it('Should throw an error if amount < 0', async () => {
+      await truffleAssert.reverts(
+        vendorInstance.buyTokensForDAI(0),
+        'Maybe you would like to buy something greater than 0?',
+      );
+    });
+
+    it('Should throw an error if balance of DAI-token at msg.sender balance is too low', async () => {
+      await truffleAssert.reverts(
+        vendorInstance.buyTokensForDAI(web3.utils.toBN(100).mul(bn1e18)),
+        'Sorry, you do not have enough DAI-tokens for swap',
+      );
+    });
+
+    it('Should throw an error if balance of SGRN-token at Vendor contract is too low', async () => {
+      await truffleAssert.reverts(
+        vendorInstance.buyTokensForDAI(web3.utils.toBN(30).mul(bn1e18)),
+        'Sorry, there is not enough tokens on my balance',
+      );
+    });
+
+    it('Should throw an error if there is not enough allowance', async () => {
+      await truffleAssert.reverts(
+        vendorInstance.buyTokensForDAI(web3.utils.toBN(3).mul(bn1e18)),
+        'Check the token allowance please',
+      );
+    });
+
+    it("Should throw an error if msg.sender is not an owner of 'key NFT token'", async () => {
+      await truffleAssert.reverts(
+        vendorInstance.buyTokensForDAI(web3.utils.toBN(3), { from: payer }),
+        "Sorry, you don't have a key to use this.",
+      );
+    });
+  });
+
+  describe('_PROXY_ Buy tokens with ETH', function () {
     it('Should buyTokens with ETH successfully', async () => {
       const tokenBalanceBefore = await testTokenInstance.balanceOf(owner);
       const vendorTokenBalanceBefore = await testTokenInstance.balanceOf(proxyInstance.address);
@@ -90,7 +204,7 @@ describe('Vendor', () => {
     });
   });
 
-  describe('Buy tokens with DAI', function () {
+  describe('_PROXY_ Buy tokens with DAI', function () {
     it('Should swap Tokens for DAI successfully', async () => {
       await testTokenInstance.transfer(proxyInstance.address, web3.utils.toBN(500).mul(bn1e18));
       await DAITokenInstance.approve(proxyInstance.address, web3.utils.toBN(50).mul(bn1e18));
