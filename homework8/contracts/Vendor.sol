a6f0d037b03c68d6561256d973184fe978793835
Sasha Grin <aleksander.g@ideasoft.io>
2021-12-20 23:53:20 +0200

    homework 9

--- homework8/contracts/Vendor.sol
+++ homework8/contracts/Vendor.sol
@@ -4,9 +4,11 @@
 pragma solidity ^0.8.10;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
@@ -32,10 +34,53 @@ interface studentsInterface {
     function getStudentsList() external view returns (string[] memory studentsList);
 }
 
+contract RandomNumberConsumer is VRFConsumerBase {
+    event RandomNumberUpdated(uint256 number);
+
+    bytes32 internal keyHash;
+    uint256 internal fee;
+
+    uint256 public randomResult;
+
+    /**
+     * Constructor inherits VRFConsumerBase
+     *
+     * Network: Kovan
+     * Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
+     * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
+     * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
+     */
+    constructor()
+    VRFConsumerBase(
+        0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
+        0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
+    )
+    {
+        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK (Varies by network)
+    }
+
+    /**
+     * Requests randomness
+     */
+    function getRandomNumber() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee);
+    }
+
+    /**
+     * Callback function used by VRF Coordinator
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        emit RandomNumberUpdated(randomness);
+        randomResult = randomness;
+    }
+
+    // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
+}
 
 
-
-contract Vendor is Initializable, OwnableUpgradeable, UUPSUpgradeable   {
+contract Vendor is Initializable, OwnableUpgradeable, UUPSUpgradeable, RandomNumberConsumer    {
     event Log(string message);
     event LogBytes(bytes message);
 
@@ -46,13 +91,13 @@ contract Vendor is Initializable, OwnableUpgradeable, UUPSUpgradeable   {
     address NFTTokenContractAddress;
     uint256 keyNftTokenId;
 
-
     modifier hasKeyNFTToken() {
         _isTokenHolder();
         _;
     }
 
-    function initialize(address tokenContractAddress, address _DAITokenContractAddress, address nftTokenContractAddress, uint256 _keyNftTokenId) public initializer {
+    function initialize(address tokenContractAddress, address _DAITokenContractAddress, address nftTokenContractAddress, uint256 _keyNftTokenId)  public initializer
+    {
         studentsContractAddress = 0x0E822C71e628b20a35F8bCAbe8c11F274246e64D;
         aggregatorAddressFor_ETH_USD = 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e;
         DAITokenContractAddress = _DAITokenContractAddress;
