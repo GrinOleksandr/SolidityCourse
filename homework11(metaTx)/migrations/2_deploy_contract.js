d4ca30b492a1252c2309d28ea423d1d131bc7d64
Sasha Grin <aleksander.g@ideasoft.io>
2022-01-07 18:42:13 +0200

    yeah, it works at all!!

--- homework11(metaTx)/migrations/2_deploy_contract.js
+++ homework11(metaTx)/migrations/2_deploy_contract.js
@@ -1,36 +1,10 @@
-// const Forwarder = artifacts.require('MinimalForwarder');
-// const Vendor = artifacts.require('Vendor');
-// // const Proxy = artifacts.require('ERC1967Proxy');
-//
-// const sgrnTokenContractAddressRinkeby = '0x58F56eFb1Bc4D0c566c493E019EE7dDcc987f720';
-// const daiTokenContractAddressRinkeby = '0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa';
-// const minimalForwarderAddressRinkeby = '0x039a437431c2fEC0903e67112973787D8046e5AD';
-//
-// module.exports = async function (deployer) {
-//   // await deployer.deploy(Forwarder);
-//
-//   await deployer.deploy(
-//     Vendor,
-//     sgrnTokenContractAddressRinkeby,
-//     daiTokenContractAddressRinkeby,
-//     minimalForwarderAddressRinkeby,
-//   );
-// };
-
 const Vendor = artifacts.require('Vendor');
 const Proxy = artifacts.require('ERC1967Proxy');
-
-// const sgrnTokenContractAddressRinkeby = '0x58F56eFb1Bc4D0c566c493E019EE7dDcc987f720';
-// const daiTokenContractAddressRinkeby = '0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa';
-// const sigmaNFTTokenAddressRinkeby = '0x515Dcbe2cCF7d159CBc7C74DA99DeeD04F671725';
-// const myNFTTokenId = 0;
 
 const sgrnTokenContractAddressKovan = '0x8e2892D455D8bbdDE5bE2D8D14A1F9416d6fa2F1';
 const daiTokenContractAddressKovan = '0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa';
 
 module.exports = async function (deployer) {
-  // await deployer.deploy(TestToken, 10000000000);
-
   await deployer.deploy(Vendor);
   const implementationInstance = await Vendor.deployed();
 
@@ -39,6 +13,4 @@ module.exports = async function (deployer) {
     .encodeABI();
 
   await deployer.deploy(Proxy, implementationInstance.address, data);
-  //
-  // await deployer.deploy(VendorV2);
 };
