0c316ccb0e419b44f009eaefdd09a8db615fd219
Sasha Grin <aleksander.g@ideasoft.io>
2022-02-02 15:55:46 +0200

    added prettier

--- homework11(metaTx)/contracts/ERC1967Proxy.sol
+++ homework11(metaTx)/contracts/ERC1967Proxy.sol
@@ -3,4 +3,4 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import '@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol';
